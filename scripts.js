e9183aae1cbffd084d428b0c40238f203f2e96f1
SimonMcCaffery <161161449+SimonMcCaffery@users.noreply.github.com>
2025-02-17 18:13:10 +0000

    commit
    added btns for numbers in html
    added functions for basic math operations

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
 console.log("Hello WOrld!");
+
+function addition (val1, val2){
+    return val1+val2;
+}
+
+function subtraction (val1, val2){
+    return val1-val2;
+}
+
+function multiply (val1, val2){
+    return val1*val2;
+}
+
+function divide (val1, val2){
+    return val1/val2;
+}
+
+console.log(multiply(2,5));
+console.log(divide(10,2));
+console.log(addition(5,5));
+console.log(subtraction(10,5));
