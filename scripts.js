cf616c60bc4eedb6b3282b2e69bdede26109a965
SimonMcCaffery <161161449+SimonMcCaffery@users.noreply.github.com>
2025-02-18 18:09:30 +0000

    styling + function
    added more styling (buttons) plus operate function and vars for user input.
    
    Rearranged buttons in html

--- scripts.js
+++ scripts.js
@@ -1,5 +1,24 @@
 console.log("Hello WOrld!");
 
+var userInputFirst;
+var userInputSecond;
+var operand;
+
+function operate(val1, val2, operator){
+    switch (operator) {
+        case "+":
+            return addition(val1, val2)            
+        case "-":
+            return subtraction(val1, val2)
+        case "*":
+            return multiply(val1, val2)
+        case "/":
+            return divide(val1, val2)
+        default:
+            break;
+    }
+}
+
 function addition (val1, val2){
     return val1+val2;
 }
@@ -16,7 +35,7 @@ function divide (val1, val2){
     return val1/val2;
 }
 
-console.log(multiply(2,5));
-console.log(divide(10,2));
-console.log(addition(5,5));
-console.log(subtraction(10,5));  
+console.log(operate(5,5,"+") + " Expect 10");  
+console.log(operate(2,9,"*") + " Expect 18");
+console.log(operate(10,5,"-") + " Expect 5");
+console.log(operate(10,5,"/") + " Expect 2");
